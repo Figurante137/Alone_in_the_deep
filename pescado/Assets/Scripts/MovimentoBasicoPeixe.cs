8fea823e8fd6f99d46994fa2f2a22161806f2b57
Vitor Henrique Silva Cezar <vvitorhenrique5@gmail.com>
2025-04-20 03:32:20 -0300

    hedede

--- pescado/Assets/Scripts/MovimentoBasicoPeixe.cs
+++ pescado/Assets/Scripts/MovimentoBasicoPeixe.cs
@@ -6,6 +6,8 @@ public class MovimentoBasicoPeixe : MonoBehaviour
     public float changeDirectionTime = 3f;
     public float swimAmplitude = 0.5f;
     public float swimFrequency = 2f;
+    public float rayDistance = 1f;
+    public LayerMask limite;
 
     private Vector2 direction;
     private float timer;
@@ -20,24 +22,35 @@ public class MovimentoBasicoPeixe : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, rayDistance, limite);
+        if (hit.collider != null)
+        {
+            //ChooseNewDirection(); // Muda a direção se bateu em um limite
+            direction = direction * -1;
+            ChooseNewDirection();
+            timer = 0f;
+        }
         // Muda a direção após um tempo
         if (timer >= changeDirectionTime)
         {
             ChooseNewDirection();
-            timer = 0;
+            timer = 0f;
         }
 
         // Movimento oscilante
         Vector2 swimOffset = new Vector2(0, Mathf.Sin(Time.time * swimFrequency) * swimAmplitude);
         transform.Translate((direction * speed * Time.deltaTime) + swimOffset * Time.deltaTime);
-
-        // Aqui eu vou adicionar detecção de borda da tela para mudar direção
     }
 
     void ChooseNewDirection()
     {
         float angle = Random.Range(0, 2 * Mathf.PI);
         direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+    }
+    void OnDrawGizmos()
+    {
+        // Gizmo para visualizar o Raycast na cena
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)(direction * rayDistance));
     }
 }
